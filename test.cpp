0b293c68053cbb65903577900d417419a6afd2fe
roosslan <open.ok@gmail.com>
2024-06-03 12:14:02 +0300

    to base on

---
+++ test.cpp
@@ -0,0 +1,70 @@
+
+
+#include "pch.h"
+#define _AFXDLL
+#include "..\winralter\stdafx.h"
+
+//#include "..\winralter\winralter.h"
+
+#include <gtest/gtest.h>
+
+#include "..\winralter\CwrHelper.h"
+
+namespace gtests {
+    namespace winralters_test {
+        //namespace winralter {
+
+            // The fixture for testing class Foo.
+            class FooTest : public testing::Test {
+            protected:
+                // You can remove any or all of the following functions if their bodies would
+                // be empty.
+
+                FooTest() {
+                    // You can do set-up work for each test here.
+                }
+
+                ~FooTest() override {
+                    // You can do clean-up work that doesn't throw exceptions here.
+                }
+
+                // If the constructor and destructor are not enough for setting up
+                // and cleaning up each test, you can define the following methods:
+
+                void SetUp() override {
+                    // Code here will be called immediately after the constructor (right
+                    // before each test).
+                }
+
+                void TearDown() override {
+                    // Code here will be called immediately after each test (right
+                    // before the destructor).
+                }
+
+                // Class members declared here can be used by all tests in the test suite
+                // for Foo.
+            };
+
+            // Tests that the Foo::Bar() method does Abc.
+            TEST_F(FooTest, GetConfigFilePath) {
+            //    const std::string input_filepath = "this/package/testdata/myinputfile.dat";
+            //    const std::string output_filepath = "this/package/testdata/myoutputfile.dat";
+                winralter::CwrHelper gwrHelper;
+                EXPECT_EQ(gwrHelper.GetConfigFilePath(0), "d0"); // 0 = PermanentConfig
+            }
+
+            // Tests that Foo does Xyz.
+            TEST_F(FooTest, DoesXyz) {
+                // Exercises the Xyz feature of Foo.
+            }
+
+ //       }  // namespace
+    }  // namespace project
+}  // namespace my
+
+int main(int argc, char** argv) {
+    testing::InitGoogleTest(&argc, argv);
+
+
+    return RUN_ALL_TESTS();
+}
